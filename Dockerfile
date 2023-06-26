a555dd12bf82b1decc6287ea756038b8735d6bc1
ManOwnFire <b.avg@hotmail.com>
2023-06-26 19:13:13 +0200

    setting new environment

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Use the official Python base image with the desired version
+FROM python:3.9
+
+# Set the working directory inside the container
+WORKDIR /backend
+
+# Copy the requirements file to the working directory
+COPY requirements.txt .
+
+
+
+# Install the Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code to the working directory
+COPY . .
+
+# Expose a port (if your application requires it)
+EXPOSE 8000
+
+# Define the command to run when the container starts
+CMD ["python3", "main.py"]
--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import time
 import paho.mqtt.subscribe as sub
 
+print("Starting Backend...")
+
 #enter infinate loop
 while True:
     # get current datetime and time
@@ -16,7 +18,7 @@ while True:
     if now > global_variables.sub_start_time and now < stop_time:
         print("Recording broker trafic subscribed to: tokenStations/+/onScan") # "+" is a wildcard
         if __name__ == '__main__':
-            broker_address = "backend.exhibition.futurium.de"  #set the Broker address
+            broker_address = "172.25.2.56"  #set the Broker address
             sub.callback(mqtt_subscribe_callback.on_msg_print, "tokenStations/+/onScan", hostname=broker_address) #functions subscribes on Mqtt Broker and calls callback function "on_msg_print" from mqtt_callback
 
     print(str(now) + "Main loop ended...")
@@ -24,3 +26,6 @@ while True:
     print("Start Time is set to :  " + str(global_variables.sub_start_time))
     print("Stop time is set to:   " + str(stop_time))
     time.sleep(60)
+
+
+
