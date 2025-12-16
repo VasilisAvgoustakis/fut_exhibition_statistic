6f2b5e9fdc28cef7ab7ed55dbf5f517804621923
VasilisAvgoustakis <b.avg@hotmail.com>
2025-12-16 17:07:00 +0100

    Removes old irrelevant files. Updates backend dependencies.

--- backend/main.py
+++ backend/main.py
@@ -8,19 +8,10 @@ import config
 from datetime import datetime
 import time
 from utils import mqtt_client, db
-import scans_to_db, assets_to_db
+import scans_to_db
 
 # Set up logging
 logger = config.setup_logging("backend")
-
-def times_parser_process_wrapper():
-    """Wrapper for the time parser process to handle exceptions."""
-    try:
-        process_times.process_scan_times()
-    except ValueError as e:
-        logger.error("ERROR occurred in process_scan_times: %s", e)
-    except Exception as e:
-        logger.exception("EXCEPTION occurred in process_scan_times: %s", e)
 
 def scan_parser_process_wrapper():
     """Wrapper for the scan parser process to handle exceptions."""
@@ -31,14 +22,6 @@ def scan_parser_process_wrapper():
     except Exception as e:
         logger.exception("EXCEPTION in process_daily_scans")
 
-def asset_parser_process_wrapper():
-    """Wrapper for the asset parser process to handle exceptions."""
-    try:
-        assets_to_db.process_daily_asset_calls()
-    except ValueError as e:
-        logger.error("ERROR occurred in process_daily_asset_calls: %s", e)
-    except Exception as e:
-        logger.exception("EXCEPTION in process_daily_asset_calls")
 
 def scan_listener_wrapper():
     """Wrapper for the MQTT scan listener to handle exceptions."""
@@ -65,17 +48,8 @@ if __name__ == '__main__':
     # Process for storing daily scans to database at the end of each day
     parse_daily_scans = Process(target=scan_parser_process_wrapper, args=())
 
-    # Process for storing daily asset calls in DB at end of each day
-    # parse_daily_asset_calls = Process(target=asset_parser_process_wrapper, args=())
-
     # Start the listener
     scan_listener_process.start()
 
     # Start the db scan parser
     parse_daily_scans.start()
-    
-    # Start the db asset parser
-    # parse_daily_asset_calls.start()
-
-    # Start the time processing
-    # process_daily_times.start()
